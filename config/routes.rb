a6d2c8c40073b9214c9edf91b8251358f5d66746
Rines.io <tsrines@gmail.com>
2020-02-22 06:21:07 -0500

    create user feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :doctors
+  resources :users
   root to: "calls#root"
+   
   resources :calls, only: :create
   mount ActionCable.server, at: '/cable'
 end
