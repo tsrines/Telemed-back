e7dc2f8520412795043840fc926fe3aeb743801d
Rines.io <tsrines@gmail.com>
2020-02-24 19:21:55 -0500

    edit profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   has_many :ailments
   has_many :doctor_ailments, through: :ailments
   has_many :doctors, through: :doctor_ailments
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true
+  # validates :email, uniqueness: true
   validates :password, presence: true
-  validates :password_confirmation, presence: true
+
 end
